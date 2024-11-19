4e2706fa8aafdb50a1f587547ab7c1616f808ece
Fabian Quintero Naranjo <fabian.naranjo@onelinkbpo.com>
2024-11-18 21:23:52 -0500

    routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+  standalone: true,  // Declaramos que el componente es independiente
+  imports: [CommonModule, RouterModule, HomeComponent, LoginComponent], // Importamos RouterModule y los componentes
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'TestDev2025Frontend';
+
+  // Definimos las rutas directamente aquí
+  routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
+    { path: '**', redirectTo: '/home' }
+  ];
 }
