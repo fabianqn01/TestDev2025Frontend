45bf9b7b969b8acf715308c0ae5718ec7fb2bafb
Fabian Quintero Naranjo <fabian.naranjo@onelinkbpo.com>
2024-11-20 15:22:08 -0500

    login logout and home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterModule], // Asegúrate de importar RouterModule aquí
+  imports: [CommonModule, RouterModule, NavbarComponent], // Asegúrate de importar RouterModule aquí
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
---
+++ src/app/navbar/components/navbar/navbar.component.ts
@@ -0,0 +1,37 @@
+import { Component, inject } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { selectUser } from '../../../auth/store/auth.selectors';
+import { AuthActions } from '../../../auth/store/auth.actions';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+  private readonly store = inject(Store);
+  private readonly router = inject(Router);
+  user$ = this.store.select(selectUser);  // Obtenemos el usuario desde el estado global
+  dropdownOpen = false;  // Para controlar si el menú desplegable está abierto o cerrado
+
+  logout() {
+    this.store.dispatch(AuthActions.logout()); // Disparamos la acción de logout
+    this.router.navigate(['/login']);  // Redirigimos a login después del logout
+  }
+
+  navigateToLogin() {
+    this.router.navigate(['/login']);  // Navegamos a la página de login
+  }
+
+  navigateToHome() {
+    this.router.navigate(['/home']);  // Navegamos al home
+  }
+
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;  // Alterna el estado del menú desplegable
+  }
+}
